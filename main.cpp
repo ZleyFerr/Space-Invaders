6b3532313b56906d77c3b4e6c170908aebb1e083
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2025-05-07 20:19:24 +0300

    font fix

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+    //загрузка шрифта
+    if (!font.loadFromFile("Fonts/game_font.ttf"))
+    {
+        std::cerr << "font error!" << std::endl;
+        return -1;
+    }
+
 	game();
 }
